cc6091eaaaa53074e458ce237381b7a426a4dd63
Paul DiBenedetto <pdibenedetto.ctr@kr.af.mil>
2020-03-09 12:45:49 -0400

    Finishing Lecture 195

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -8,7 +8,8 @@ import  axios from "axios";
 
 class Blog extends Component {
     state = {
-        posts: []
+        posts: [],
+        selectedPostId: null
     };
 
     componentDidMount() {
@@ -26,9 +27,13 @@ class Blog extends Component {
             });
     }
 
+    postSelectedHandler = (id) => {
+      this.setState({selectedPostId: id})
+    };
+
     render () {
         const posts = this.state.posts.map(post => {
-            return <Post key={post.id} title={post.title} author={post.author}/>
+            return <Post key={post.id} title={post.title} author={post.author} clicked={() => this.postSelectedHandler(post.id)}/>
             }
         );
         return (
@@ -37,7 +42,7 @@ class Blog extends Component {
                     {posts}
                 </section>
                 <section>
-                    <FullPost />
+                    <FullPost id={this.state.selectedPostId}/>
                 </section>
                 <section>
                     <NewPost />
